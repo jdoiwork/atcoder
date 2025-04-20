cc361f2c65c30103964bf19743d38a1269fbf312
jdoi <jdoi.work@gmail.com>
2025-04-21 07:53:36 +0900

    arc096-a

--- arc096/a/Main.hs
+++ arc096/a/Main.hs
@@ -7,11 +7,9 @@ main :: IO ()
 main = do
   [a, b, c, x, y] <- readNumbers
   let n = min x y
-      m = (n `div` 2)
-      n2 = m * 2
-      x' = x - n2
-      y' = y - n2
-      p2 = (pair2 (a, b, c, x, y)) * m
+      x' = x - n
+      y' = y - n
+      p2 = (pair2 (a, b, c, x, y)) * n
       p1 = pair1 (a, b, c, x', y')
       answer = p1 + p2
 
@@ -25,17 +23,11 @@ type PizzaSet = (IntX, IntX, IntX, IntX, IntX)
 pair2 :: PizzaSet -> IntX
 pair2 (a, b, c, _x, _y) = min ab cc
   where
-    ab = (a + b) * 2
-    cc = c * 4
+    ab = (a + b)
+    cc = c * 2
 
 pair1 :: PizzaSet -> IntX
-pair1 (a, b, c, x, 0) = (min a (c * 2)) * x
-pair1 (a, b, c, 0, y) = (min b (c * 2)) * y
-pair1 (a, b, c, x, y) = min ab cc
+pair1 (a, b, c, x, y) = sx + sy
   where
-    ab = a * x + b * y
-    cc = (c * 2) * (max x y)
--- pair1 (a, b, c, x, y) = min ab cc
---   where
---     ab = a * x + b * y
---     cc = (c * 2) * (max x y)
+    sx = (min a (c * 2)) * x
+    sy = (min b (c * 2)) * y
