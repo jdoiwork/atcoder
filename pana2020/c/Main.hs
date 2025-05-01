3f026293a26955e8d84894df5b43b25dcf22d07a
jdoi <jdoi.work@gmail.com>
2025-05-02 00:55:50 +0900

    pana2020-c tmp

---
+++ pana2020/c/Main.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text.Lazy.IO as T
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Read as T
+
+-- import System.IO (hPrint, stderr)
+
+type IntX = Integer
+
+main :: IO ()
+main = do
+  [a, b, c] <- readNumbers
+
+  -- hPrint stderr $ (a, b, c)
+  -- 4 * a * b < (c - a - b) ^ 2
+
+  T.putStrLn $ if (4 * a * b) < ((c - a - b) ^ (2 :: IntX))
+    then "Yes"
+    else "No"
+
+readNumbers :: IO [IntX]
+readNumbers = map readDecimal . T.words <$> T.getLine
+  where
+    readDecimal t = case T.signed T.decimal t of
+      Right (!n, _) -> n
+      Left e -> error $ "Invalid Input: " ++ show e
